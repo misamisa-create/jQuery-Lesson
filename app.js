e8fd811651f50819cbd1482634e0ad1ad6837209
松山実紗 <matsuyamajitsusha@matsuyamajitsushanoMacBook-puro.local>
2021-07-24 13:48:50 +0900

    [Finish]js-7

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function () {
+  $('button').on('click', function () {
+    $('ul').children().css('color', 'red');
+  });
+
+
 });
+ 
